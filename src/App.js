02912cd2c718b5ee4f9b7ed8622569f44ce84c5e
GogiKalinin <bbdostoevskij@gmail.com>
2023-02-22 17:24:30 +0200

    add 5 days weather forecast api data

--- src/App.js
+++ src/App.js
@@ -27,13 +27,40 @@ let city = 'moscow'
     setInterval(() => {
       init()
     }, 1000000) //Обновляет инфу каждые 1000секунд
+
+
+    let town = 'moscow';
+  function init2() {
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${town}&appid=d982b206b7125a363d94918d08ebf560`)
+    .then((resp) => {return resp.json()})
+    .then((data) => {
+
+      console.log('data', data)
+      console.log('city', data.name)
+      
+      localStorage.setItem('localData', JSON.stringify(data))
+  })
+
+    .catch(() => {
+        init2()
+      })
+    }
     
-  console.log(JSON.parse(localStorage.getItem('localData')))
+    init2()
+    
+    setInterval(() => {
+      init2()
+    }, 1000000) //Обновляет инфу каждые 1000секунд
+    
+  // console.log(JSON.parse(localStorage.getItem('localData')))
   const [stateData, setStateData] = useState(JSON.parse(localStorage.getItem('localData')))
-  console.log(stateData)
+  // console.log(stateData)
     return (
       <div className="App">
-        <MainPage data={stateData}/>
+        {/* <MainPage data={stateData}/> */}
+        <a href="https://www.pexels.com">
+          <img src="https://images.pexels.com/lib/api/pexels-white.png" />
+        </a>
       </div>
     );
 }
